d1b186bb9c373e2e629c43bedd64beb9d0a45807
Urias-Flores <floresmeza808@gmail.com>
2024-04-19 11:45:52 -0600

    Adding navigation bar effect

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
-import {
-  Form,
-  Link,
-  useLocation,
-} from "@remix-run/react";
+import { Form, Link, useLocation } from "@remix-run/react";
 
 export default function Navigation({ user, showMenu, setShowMenu }: any) {
   const { pathname } = useLocation();
   const [showList, setShowList] = useState(false);
+  const [selectedNavItems, setSelectedNavItems] = useState(0);
 
   return (
     <header>
@@ -30,69 +27,142 @@ export default function Navigation({ user, showMenu, setShowMenu }: any) {
 
         <nav className={`nav ${showMenu ? "active" : ""}`}>
           <Link
-            className={`link ${pathname === "/" ? "active" : ""}`}
+            className={`link`}
             to="/"
             onClick={() => {
               setShowMenu(false);
             }}
+            onMouseEnter={() => {
+              setSelectedNavItems(1);
+            }}
+            onMouseLeave={() => {
+              setSelectedNavItems(0);
+            }}
           >
-            Inicio
+            <p className="link__text">Inicio</p>
+            <div
+              className={`link__line ${
+                pathname === "/" || selectedNavItems === 1 ? "active" : ""
+              }`}
+            ></div>
           </Link>
 
           <Link
-            className={`link ${pathname.includes("/clientes") ? "active" : ""}`}
+            className={`link`}
             to="/clientes"
             onClick={() => {
               setShowMenu(false);
             }}
+            onMouseEnter={() => {
+              setSelectedNavItems(2);
+            }}
+            onMouseLeave={() => {
+              setSelectedNavItems(0);
+            }}
           >
-            Clientes
+            <p className="link__text">Clientes</p>
+            <div
+              className={`link__line ${
+                pathname.includes("/clientes") || selectedNavItems === 2
+                  ? "active"
+                  : ""
+              }`}
+            ></div>
           </Link>
 
           {user?.AccessLevel === "A" || user?.AccessLevel === "R" ? (
             <Link
-              className={`link ${
-                pathname.includes("/usuarios") ? "active" : ""
-              }`}
+              className={`link`}
               to="/usuarios"
               onClick={() => {
                 setShowMenu(false);
               }}
+              onMouseEnter={() => {
+                setSelectedNavItems(3);
+              }}
+              onMouseLeave={() => {
+                setSelectedNavItems(0);
+              }}
             >
-              Usuarios
+              <p className="link__text"> Usuarios</p>
+              <div
+                className={`link__line ${
+                  pathname.includes("/usuarios") || selectedNavItems === 3
+                    ? "active"
+                    : ""
+                }`}
+              ></div>
             </Link>
           ) : null}
 
           <Link
-            className={`link ${pathname.includes("/citas") ? "active" : ""}`}
+            className={`link`}
             to="/citas"
             onClick={() => {
               setShowMenu(false);
             }}
+            onMouseEnter={() => {
+              setSelectedNavItems(4);
+            }}
+            onMouseLeave={() => {
+              setSelectedNavItems(0);
+            }}
           >
-            Citas
+            <p className="link__text">Citas</p>
+            <div
+              className={`link__line ${
+                pathname.includes("/citas") || selectedNavItems === 4
+                  ? "active"
+                  : ""
+              }`}
+            ></div>
           </Link>
 
           <Link
-            className={`link ${pathname.includes("/materias") ? "active" : ""}`}
+            className={`link`}
             to="/materias"
             onClick={() => {
               setShowMenu(false);
             }}
+            onMouseEnter={() => {
+              setSelectedNavItems(5);
+            }}
+            onMouseLeave={() => {
+              setSelectedNavItems(0);
+            }}
           >
-            Materias
+            <p className="link__text">Materias</p>
+            <div
+              className={`link__line ${
+                pathname.includes("/materias") || selectedNavItems === 5
+                  ? "active"
+                  : ""
+              }`}
+            ></div>
           </Link>
 
           <Link
-            className={`link ${
-              pathname === "/documentacioninterna" ? "active" : ""
-            }`}
+            className={`link`}
             to="/documentacioninterna"
             onClick={() => {
               setShowMenu(false);
             }}
+            onMouseEnter={() => {
+              setSelectedNavItems(6);
+            }}
+            onMouseLeave={() => {
+              setSelectedNavItems(0);
+            }}
           >
-            Doc. interna
+            <p className="link__text">Doc. Interna</p>
+            <div
+              className={`link__line ${
+                pathname.includes("/documentacion-interna") ||
+                selectedNavItems === 6
+                  ? "active"
+                  : ""
+              }`}
+            ></div>
           </Link>
 
           <div className="current-user">
@@ -104,21 +174,20 @@ export default function Navigation({ user, showMenu, setShowMenu }: any) {
                 setShowList(!showList);
               }}
             />
-            {showList && (
-              <div className="dropdownlist">
-                <div className="info">
-                  <p className="username">{user?.Name}</p>
-                  <p className="email">{user?.Email}</p>
-                </div>
 
-                <Form className="logout" method="post" action="/">
-                  <button className="button" type="submit">
-                    <img src="/img/logout.svg" alt="logout" />
-                    <p>Cerrar sesión</p>
-                  </button>
-                </Form>
+            <div className={`dropdownlist ${ showList && 'active' }`}>
+              <div className="info">
+                <p className="username">{user?.Name}</p>
+                <p className="email">{user?.Email}</p>
               </div>
-            )}
+
+              <Form className="logout" method="post" action="/">
+                <button className="button" type="submit">
+                  <img src="/img/logout.svg" alt="logout" />
+                  <p>Cerrar sesión</p>
+                </button>
+              </Form>
+            </div>
           </div>
         </nav>
       </div>
